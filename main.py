b160ac7bb32fe5416f00af7810a874504a782dde
gle8098 <gle8098@yandex.ru>
2019-05-03 14:18:23 +0300

    Skeleton init

---
+++ main.py
@@ -0,0 +1,19 @@
+class Game:
+    _inst = None
+
+    @staticmethod
+    def inst():
+        if Game._inst is None:
+            Game._inst = Game()
+        return Game._inst
+
+    def init(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    Game.inst().init()
+    Game.inst().run()
